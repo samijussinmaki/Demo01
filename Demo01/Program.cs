e9b45dacedf2464aa035e1b0f40795297731f5ac
K2469 <K2469@student.jamk.fi>
2017-01-19 09:48:58 +0200

    nyt

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -10,7 +10,7 @@ namespace Demo01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("helllllooooo");
+            Console.WriteLine("helllllooo");
         }
     }
 }
